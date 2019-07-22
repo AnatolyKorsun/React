eff1e56f320d9f35fafe544b2d6973fc04955aa5
AnatolyKorsun <korsoon@i.ua>
2019-07-23 00:40:23 +0300

    add requsites

--- create-react-app/my-app/src/App.js
+++ create-react-app/my-app/src/App.js
@@ -11,15 +11,107 @@
 // export default App
 
 
+// import React, { Component } from 'react'
+// import Table from './Table'
+// class App extends Component {
+//     render() {
+//         return(
+//             <div className ="conteiner">
+//                 <Table />
+//             </div>
+//         )
+//     }
+// }
+// export default App
+
+
+// import React, { Component } from 'react'
+// const TableHeader = () => {
+//     return (
+//         <thead>
+//         <tr>
+//           <th>Name</th>
+//           <th>Job</th>
+//         </tr>
+//       </thead> 
+//     )
+// }
+// const TableBody = () => {
+//     return (
+//         <tbody>
+//       <tr>
+//         <td>Charlie</td>
+//         <td>Janitor</td>
+//       </tr>
+//       <tr>
+//         <td>Mac</td>
+//         <td>Bouncer</td>
+//       </tr>
+//       <tr>
+//         <td>Dee</td>
+//         <td>Aspiring actress</td>
+//       </tr>
+//       <tr>
+//         <td>Dennis</td>
+//         <td>Bartender</td>
+//       </tr>
+//     </tbody>  
+//     )
+// }
+// class TableElem extends Component {
+//     render() {
+//       return (
+//         <table>
+//           <TableHeader />
+//           <TableBody />
+//         </table>
+//       )
+//     }
+//   }
+// export default TableElem
+
+
 import React, { Component } from 'react'
-import Table from './Table'
-class App extends Component {
-    render() {
-        return(
-            <div className ="conteiner">
-                <Table />
-            </div>
-        )
-    }
+const TableHeader = () => {
+    return (
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead> 
+    )
 }
-export default App
+const TableBody = () => {
+    return <tbody />
+    
+    
+}
+class TableRequisite extends Component {
+    render() {
+        const characters = [
+            {
+                name: 'Charlie',
+                job: 'Janitor',
+              },
+              {
+                name: 'Mac',
+                job: 'Bouncer',
+              },
+              {
+                name: 'Dee',
+                job: 'Aspring actress',
+              },
+              {
+                name: 'Dennis',
+                job: 'Bartender',
+              },  
+        ]
+      return (
+          <div className="container">
+              <Table characterData = {characters} />
+            </div>
+      )
+    }
+  }
+export default TableRequisite
--- create-react-app/my-app/src/index.js
+++ create-react-app/my-app/src/index.js
@@ -20,8 +20,15 @@
 // ReactDOM.render(<App />, document.getElementById('root'))
 
 
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import './index.css'
+// import App from './App'
+// ReactDOM.render(<App />, document.getElementById('root'))
+
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
-ReactDOM.render(<App />, document.getElementById('root'))
+import TableElem from './App'
+ReactDOM.render(<TableElem />, document.getElementById('root'))
